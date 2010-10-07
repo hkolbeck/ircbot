fb95a2cf043a506fb77850d5669a41414008c33c
Cory D Kolbeck <cbeck@gottbrath.cs.pdx.edu>
2010-10-07 10:21:14 -0700

    add copyright notice

--- example.go
+++ example.go
@@ -1,3 +1,9 @@
+//Copyright 2010 Cory Kolbeck <ckolbeck@gmail.com>.
+//So long as this notice remains in place, you are welcome 
+//to do whatever you like to or with this code.  This code is 
+//provided 'As-Is' with no warrenty expressed or implied. 
+//If you like it, and we happen to meet, buy me a beer sometime
+
 package main
 
 import irc "cbeck/ircbot"
