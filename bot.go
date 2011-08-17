4b308a284684a5843acc6998d430adb72f8a81a1
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2011-08-17 11:03:46 -0700

    Compiling is good, I like compiling.

--- bot.go
+++ bot.go
@@ -11,7 +11,7 @@ import (
 	"os"
 	"regexp"
 	"strings"
-	"log"
+	//"log"
 	"sync"
 	"runtime"
 )
@@ -19,14 +19,18 @@ import (
 type Bot struct {
 	Nick string
 	Actions map[string]func(*Bot, *Message) *Message
-	Prefix byte
-	networks map[string]*Network
+	Attention byte
+	network *Network
 	myPrefix string
 }
 
-
 //Return a bot which stays connected and nothing else
-func NewBot(nick string, prefix byte) (bot *Bot) {
+func NewBot(nick, pass, domain, server string, port int, ssl bool, prefix byte) (*Bot, os.Error) {
+	net, err := Dial(server, port, nick, pass, domain, ssl);
+	if err != nil {
+		return nil, err
+	}
+
 	actions := map[string]func(*Bot, *Message) *Message {
 		"PING" : pong,
 		"JOIN" : join,
@@ -75,7 +79,23 @@ func NewBot(nick string, prefix byte) (bot *Bot) {
 		"ISON" : doNothing,
 	}
 
-	return &Bot{Nick : nick, Prefix : prefix, Actions : actions, networks : make(map[string]*Network, 10)}
+	bot := &Bot{
+	Nick : nick, 
+	Attention : prefix, 
+	Actions : actions, 
+	network : net,
+	myPrefix : "",
+	}
+
+	go bot.run()
+	return bot, nil
+}
+
+func (bot *Bot) JoinChannel(channel, pass string) {
+	bot.network.Out <- &Message{
+	Command : "JOIN",
+	Args : []string{channel, pass},
+	}
 }
 
 func join(bot *Bot, msg *Message) *Message {
@@ -83,126 +103,88 @@ func join(bot *Bot, msg *Message) *Message {
 	return nil
 }
 
-func (this *Bot) Send(server string, msg *Message) os.Error {
-	ntwrk, ok := this.networks[server]
-	
-	if !ok {
-		return os.NewError(fmt.Sprintf("Error: Attempted send to unknown network: %s", server))
-	}
-
-	ntwrk.Out <- msg
-
+func resetTimeout(bot *Bot, msg *Message) *Message {
+	bot.network.keepalive <- 1
 	return nil
 }
 
-func (this *Bot) Connect(server string, port int, joinChans []string) (joined int, err os.Error) {
-	cfg := &Config {
-	Address : server,
-	Port : port,
-	NickName : this.Nick,
-	QuitMessage : this.Nick,
-        }
+func (bot *Bot) run()  {
+	defer bot.network.HangUp()
+	
+	for msg := range bot.network.In {
+		go bot.dispatch(msg) 
+	}
+}
 
-	ntwrk := NewNetwork(cfg);
 
-	//Connect to server
-	if err = ntwrk.Open(); err != nil {
-		return 0, err
+//Invokes the message handler and performs pagination
+//Of the reply if needed
+func (bot *Bot) dispatch(msg *Message) {
+	defer RecoverWithTrace()
+	var reply *Message
+	
+
+	if replyFactory, ok :=  bot.Actions[msg.Command]; ok {
+		reply = (replyFactory)(bot, msg)
+	} else {
+		return //No handler defined for this message type
 	}
 
-	//Wait for USER and NICK commands to be received and processed
-	for m := range ntwrk.In {
-		if m.Command[0] == '3' || m.Command == "MODE" {
-			break
-		} else if m.Command == "433" {//Replaceme with constant
-			return 0, os.NewError("Error: Nickname already in use")
-		} else if m.Command[0] == '4' {
-			return 0, os.NewError(fmt.Sprintf("Error %s in USER or NICK command", m.Command))
-		}
+	if reply == nil {
+		return //Nothing more to do if no reply needs sending
 	}
-
-	//Join specified channels
-	for _, chn := range joinChans {
-		info.Printf("[I] Attempting to join: %s\n", chn)
-		ntwrk.Out <- &Message{
-		Command : "JOIN",
-		Args : []string{chn},
-		}
-
-		for m := range ntwrk.In {
-			if m.Command[0] == '3' {
-				joined++
-				break
-			} else if m.Command[0] == '4' {
-				break
+	
+	
+	//All messages limited to 512 chars, figure out how long
+	//Body of message can be
+	maxTrailing := bot.getTrailingMaxLength(msg)
+	
+	//Report error if prefix + command + args is too long for one message
+	if maxTrailing < 0 {
+		//errors.Printf("[E] Preamble longer than 512 chars, message not sent: %v", msg)
+		return
+	} else if maxTrailing == 0 && len(reply.Trailing) > 0 { //Or if the message has no room for a trailing segment
+		//errors.Printf("[E] Preamble leaves no room for message, message not sent: %v", msg)
+		return
+	}
+	
+	//Newlines in messages may cause issues, break the message on newlines and 
+	//send each piece separately
+	messages := strings.Split(strings.TrimSpace(reply.Trailing), "\n")
+	
+	
+	for _, m := range messages {
+		if len(m) <= maxTrailing { //If message can be sent in one go, do it
+			bot.network.Out <- &Message{
+			Prefix : reply.Prefix, 
+			Command : reply.Command, 
+			Args : reply.Args, 
+			Trailing : m,
+			}
+		} else {//Otherwise break it up into smaller pieces where len <= maxTrailing
+			for s, e := 0, maxTrailing; s < len(m) - 1; {
+				lastBreak := strings.LastIndex(m[s:e], " ") //Try not to split mid word if possible
+				
+				if lastBreak != -1 && e != len(m){
+					e = lastBreak + s
+				}
+				
+				bot.network.Out <- &Message{
+				Prefix : reply.Prefix, 
+				Command : reply.Command, 
+				Args : reply.Args, 
+				Trailing : m[s:e],
+				}
+				s = e
+				e = min(e + maxTrailing, len(m))
 			}
 		}
 	}
-
-	this.networks[server] = ntwrk
-
-	go this.run(ntwrk)
-
-	return
 }
 
-func (this *Bot) run(ntwrk *Network)  {
-	defer ntwrk.Close()
-	
-	for incMsg := range ntwrk.In {
 
-		if replyFactory, ok :=  this.Actions[incMsg.Command]; ok {
-			go func(msg *Message) {
-				defer RecoverWithTrace()
-				
-				reply := (replyFactory)(this, msg)
-				
-				//All messages limited to 512 chars, figure out how long
-				//Body of message can be
-				maxTrailing := this.getTrailingMaxLength(msg)
-				
-				//And report error if prefix + command + args is too long for one message
-				//Don't report an error if there's no room for a trailing segment, some
-				//messages don't require one.  We handle a 0 case further down.
-				if maxTrailing < 0 {
-					errors.Printf("[E] Preamble longer than 512 chars, message not sent: %v", msg)
-					return
-				}
-				
-				if msg != nil && reply != nil {
-					//Newlines in messages may cause issues, break the message on newlines and 
-					//send each piece separately
-					messages := strings.Split(strings.TrimSpace(reply.Trailing), "\n", -1)
-				
-					//Theres a message to be sent, but no room to send it, report error
-					if maxTrailing == 0 && len(reply.Trailing) > 0{
-						errors.Printf("[E] Preamble leaves no room for message, message not sent: %v", msg)
-						return
-					}
 
-					for _, m := range messages {
-						if len(m) <= maxTrailing { //If message can be sent in one go, do it
-							ntwrk.Out <- &Message{reply.Prefix, reply.Command, reply.Args, m}
-						} else {//Otherwise break it up into smaller pieces
-							for s, e := 0, maxTrailing; s < len(m) - 1; {
-								lastBreak := strings.LastIndex(m[s:e], " ") //Try not to split mid word if possible
-								
-								if lastBreak != -1 && e != len(m){
-									e = lastBreak + s
-								}
-								
-								info.Printf("[I] len(m): %v s: %v e: %v", len(m), s, e)
-								ntwrk.Out <- &Message{reply.Prefix, reply.Command, reply.Args, m[s:e]}
-								s = e
-								e = min(e + maxTrailing, len(m))
-							}
-						}
-					}
-				}
-			}(incMsg)
-		}
-	}
-}
+
 
 func min(a, b int) int {
 	if a > b {
@@ -211,11 +193,11 @@ func min(a, b int) int {
 	return a
 }
 
-func (this *Bot) getTrailingMaxLength(msg *Message) int {
-	maxLength := 512 //Defined in spec
+func (bot *Bot) getTrailingMaxLength(msg *Message) int {
+	maxLength := 512 //Defined in rfc1459
 	usedLength := 2//For \r\n
 
-	usedLength += len(this.myPrefix) + 2
+	usedLength += len(bot.myPrefix) + 2
 	
 	if (msg.Command != "") {
 		usedLength += len(msg.Command) + 1
@@ -233,23 +215,23 @@ func (this *Bot) getTrailingMaxLength(msg *Message) int {
 }
 
 
-func (this *Bot) SetPrivmsgHandler(handler, other func(string, *Message) string) {
+func (bot *Bot) SetPrivmsgHandler(handler, other func(string, *Message) string) {
 
 	//Create a regex to match in-channel messages of the form 'botnick: blah'
 	//Match the bot's nick, followed by any char not legal in an irc nick, possibly followed by some number of spaces or tabs.
 	//Legal chars are, without the escapes below: a-zA-Z0-9[]{}\|^`-_
-	regex := regexp.MustCompile(fmt.Sprintf(`^%s[^a-zA-Z0-9\[\]{}\\\|\^\-_][ \t` + "`]*", this.Nick))
+	regex := regexp.MustCompile(fmt.Sprintf(`^%s[^a-zA-Z0-9\[\]{}\\\|\^\-_][ \t` + "`]*", bot.Nick))
 	
-	this.Actions["PRIVMSG"] =
+	bot.Actions["PRIVMSG"] =
 		func(bot *Bot, msg *Message) *Message {
 		
 		var query, reply, target string
 		
-		if msg.Args[0] == this.Nick { //Private message
+		if msg.Args[0] == bot.Nick { //Private message
 			target = msg.GetSender()
 			query = msg.Trailing[0:]
 			reply = handler(query, msg)
-		} else if msg.Trailing[0] == this.Prefix { //Message using prefix
+		} else if msg.Trailing[0] == bot.Attention { //Message using attention char
 			target = msg.Args[0]
 			query = msg.Trailing[1:]
 			reply = handler(query, msg)
@@ -292,7 +274,7 @@ func doNothing(bot *Bot, msg *Message) *Message {
 
 func RecoverWithTrace() {
 	if x := recover(); x != nil {
-		errors.Printf("[***] Runtime Panic caught: %v\n", x)
+		fmt.Printf("[***] Runtime Panic caught: %v\n", x)
 		
 		var btSync sync.Mutex
 		btSync.Lock()
@@ -309,7 +291,7 @@ func RecoverWithTrace() {
 			}
 			
 			f := runtime.FuncForPC(pc)
-			errors.Printf("[***]---> %d(%s): %s:%d\n", i-1, f.Name(), file, line)
+			fmt.Printf("[***]---> %d(%s): %s:%d\n", i-1, f.Name(), file, line)
 			i++
 		}
 		
--- network.go
+++ network.go
@@ -8,26 +8,35 @@ package ircbot
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"net"
 	"os"
+	"regexp"
 	"runtime"
+	"time"
 	"crypto/tls"
 )
 
 const (
+	//Times in nanoseconds
 	ReconnectDelay = 5e9
 	KeepAliveInterval = 10e9
+	PingTimeout = 5e9
+	ReadTimeout = 5e9
+	
 	CommBufferSize = 16
 	nickserv = `NickServ`
 )
 
-type Network {
-	conn net.TCPConn  //Connection to the irc server 
-	io bufio.ReadWriter //Wraps conn's Read/Write operations
-	In <-chan *Message //Channel containing messages the bot recieves
-	Out chan<- *Message //Channel containing messages to be sent
+type Network struct {
+	conn net.Conn  //Connection to the irc server 
+	io *bufio.ReadWriter //Wraps conn's Read/Write operations
+	In chan *Message //Channel containing messages the bot recieves
+	Out chan *Message //Channel containing messages to be sent
 	disconnect chan int64 //Internal channel to signal a keepalive failure
 	keepalive chan int //The bot's PONG command should send a value down this channel
+	running bool //Flag polled by goroutines to determine if the should continue running
 }
 
 func Dial(addr string, port int, nick, pass, domain string, ssl bool) (*Network, os.Error) {
@@ -38,61 +47,79 @@ func Dial(addr string, port int, nick, pass, domain string, ssl bool) (*Network,
 		//Will keep retrying forever at intervals of ReconnectDelay nanoseconds
 		//This is seriously racey, and could certainly result in lost messages
 		go func() {
-			for {
+			for network.running {
 				<-network.disconnect
 				network.conn.Close()
 				
-				if 	newNet, err := dial(addr, port, nick, pass, domain, ssl); err != nil {
+				if newNet, err := dial(addr, port, nick, pass, domain, ssl); err != nil {
 					//TODO: Loggit
 					network.disconnect <- (<-time.After(ReconnectDelay))
 					continue
+				} else {
+					*network = *newNet
 				}
-				
-				*network = *newNet
 			}
+			network.conn.Close()
 				
-		}
+		}()
 	}
 	
 	return network, err
 }
 
-func dial(server string, port int, nick, domain string, ssl bool) (*Network, os.Error) {
+func dial(server string, port int, nick, pass, domain string, ssl bool) (*Network, os.Error) {
+	var tcpConn *net.TCPConn
 	var conn net.Conn
 	var err os.Error
 	var resp []byte
-	
+	var network *Network
+	errRegex, _ := regexp.Compile(`[^ \n\r]+ 4[0-9][0-9]`)
+
 	addr, err := net.ResolveTCPAddr("tcp", server + string(port))
 	if err != nil {
 		goto Error
 	}
 	
-	if conn, err = net.DialTCP("tcp", nil, addr); err != nil {
+	if tcpConn, err = net.DialTCP("tcp", nil, addr); err != nil {
 		goto Error
 	}
+
+	if err = tcpConn.SetKeepAlive(true); err != nil {
+		goto Error
+	}
+
+	if err = tcpConn.SetReadTimeout(ReadTimeout); err != nil {
+		goto Error
+	}	
 
 	if ssl {
 		conn = tls.Client(conn, nil) //Let's try a nil config..
+	} else {
+		conn = tcpConn
 	}
-
-	if err = conn.SetKeepAlive(true); err != nil {
-		goto Error
-	}
-
-	if err = conn.SetReadTimeout(ReadTimeout); err != nil {
-		goto Error
-	}	
 	
-	network := &Network{conn, bufio.NewReadWriter(conn, conn), false, make(<-chan *Message, RW_BUFF), make(chan<- *Message, RW_BUFF)}
+	network = &Network{
+	conn : conn, 
+	io : bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn)), 
+	In : make(chan *Message, CommBufferSize), 
+	Out : make(chan *Message, CommBufferSize),
+	disconnect : make(chan int64),
+	keepalive : make(chan int),
+	running : true,
+	}
 
 	network.io.WriteString(fmt.Sprintf("USER %s %s %s :%s\n\r", nick, domain, addr, nick))
 	network.io.WriteString("NICK " + nick + "\n\r")
 
+	
+
 	//Check for connection/nick errors - this will consume first line of motd if no error occurs
-	if resp, _, err = network.io.ReadLine(); err == nil && regexp.Match(`[^ \n\r]+ 4[0-9][0-9]`, resp) {
+	if resp, _, err = network.io.ReadLine(); err == nil && errRegex.Match(resp) {
 		//Nick already taken, try a ghost kill
-		network.io.WriteString(fmt.Sprintf("PRIVMSG %s :ghost %s %s\n\r", nickserv, nick, pass)
-		if resp, err = network.io.ReadString(); err == nil && strings.Index(resp, `killed`) != -1 {
+		network.io.WriteString(fmt.Sprintf("PRIVMSG %s :ghost %s %s\n\r", nickserv, nick, pass))
+		if resp, _, err = network.io.ReadLine(); err == nil && 
+			bytes.Index(resp, []byte(`killed`)) != -1 {
+			
 			//Looks like it worked, try again
 			network.io.WriteString("NICK " + nick + "\n\r")
 			//Need another check here?
@@ -105,19 +132,19 @@ func dial(server string, port int, nick, domain string, ssl bool) (*Network, os.
 	}
 	
 	//Consume motd
-	for network.io.Buffered() > 0 {
+	for network.io.Reader.Buffered() > 0 {
 		network.io.ReadLine()
 	}
 	
 	if pass != "" {
-		network.io.WriteString("PRIVMSG %s :identify %s\n\r", nickserv, pass)
+		network.io.WriteString(fmt.Sprintf("PRIVMSG %s :identify %s\n\r", nickserv, pass))
 	}
 	
 	//Might fail here if pass is wrong, but we'll let the user deal
 	
 	go network.listen()
 	go network.speak()
-	go network.keepAlive()
+	go network.keepAlive(nick)
 		
 	return network, nil
 	
@@ -126,31 +153,35 @@ Error:
 	return nil, err
 }
 
-func (self *Network) keepAlive() {
+func (self *Network) HangUp() {
+	self.running = false
+}
+
+func (self *Network) keepAlive(nick string) {
 	tick := time.NewTicker(KeepAliveInterval)
 	defer tick.Stop()
 		
-	for {
+	for self.running {
 		<-tick.C
 		self.Out <- &Message{
 			Command : "PING",
-			Args : []string{nick}
+			Args : []string{nick},
 		}
-		timeout := time.After(interval + 5e9)
+		timeout := time.After(PingTimeout)
 		select {
-			<-timeout: self.disconnect <- 1 
-			<-self.keepalive: continue
+			case <-timeout: self.disconnect <- 1
+			case <-self.keepalive: continue
 		}
 	}
 }
 
 func (self *Network) listen() {
-	for {
+	for self.running {
 		msg, _, err := self.io.ReadLine()
 		if err != nil {
 			//TODO: Log failure
 			//During disconnection, this could spin, make sure reconnect runs
-			runtime.GoSched() 
+			runtime.Gosched() 
 			continue
 		}
 		self.In <- Decode(msg)
@@ -158,15 +189,16 @@ func (self *Network) listen() {
 }
 
 func (self *Network) speak() {
-	for {
-		msg := Encode(<-self.Out)
-		err, _ := self.io.Write(msg)
+	
+	for self.running {
+		msg := <-self.Out
+		_, err := self.io.Write(msg.Encode())
 		
 		//If write fails, keep trying
 		for err != nil {
 			//During disconnection, this could spin, make sure reconnect runs
-			runtime.GoSched()
-			err, _ := self.io.Write(msg)		
+			runtime.Gosched()
+			_, err = self.io.Write(msg.Encode())		
 		}
 		
 	}
