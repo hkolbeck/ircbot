072dcd6ce7660617ce284bf116b48ef7599d8da1
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2011-08-18 00:54:36 -0700

    Add a ctcp to the example

--- example.go
+++ example.go
@@ -9,14 +9,18 @@ package main
 import (
 	irc "cbeck/ircbot"
 	"fmt"
+	"os"
 )
 
 //Implement a very simple bot which will respond to anyone addressing it and join
 //any channels it is invited to.
+var bot *irc.Bot
 
 func main() {
+	var err os.Error
+	
 	//Create a new bot
-	bot, err := irc.NewBot(
+	bot, err = irc.NewBot(
 		"goirc-bot", //The bot's nick
 		"", //The bot's nickserv password (blank for none)
 		"www.github.com",  //The bot's domain
@@ -37,18 +41,15 @@ func main() {
 	// `func(bot *irc.Bot, msg *irc.Message) *irc.Message`
 	bot.Actions["INVITE"] = join
 
-	//PRIVMSG can be handled as above, or you can use the convenience method below
-	//It accepts two functions, the first arguemnt will be called for messages directed
+	//PRIVMSG can be handled as above, or you can use the convenience method below.
+	//It accepts two functions, the first will be called for messages directed
 	//to the bot via its attention char, addressing it by name, or by sending it a 
-	//private message.
-	//The second will be called for all other messages in 
-	bot.SetPrivmsgHandler(sayHi, nil)
-
+	//private message.  The second will be called for all other messages
+	bot.SetPrivmsgHandler(sayHi, ctcpEcho)
 
 	//Connect to the server on the given port, and join any channels specified
 	//Returns the number of channels joined (which we're ignoring) and an error if any
 	bot.JoinChannel("#echo", "")
-
 
 	//No further work to be done in main, block indefinitely
 	select {}
@@ -56,9 +57,40 @@ func main() {
 
 //Whether the bot is addressed with its attention char, in a private message, or with example-bot:
 //cmd will hold the meaningful part of the message
-//msg holds the raw irc message broken into prefix, command, args, and trailing 
+//msg holds the raw irc message broken into prefix, command, args, trailing, and possibly a CTCP command
 func sayHi(cmd string, msg *irc.Message) string {
+	if msg.Ctcp == "VERSION" {
+		return "GoIRC example bot 1.0"
+	}
+	
 	return "Hi there, " + msg.GetSender()
+}
+
+//This will be called for any message not directed at the bot
+//Here, we just listen for any CTCP actions and copy them 
+func ctcpEcho(cmd string, msg *irc.Message) string {
+	//The convenience methods are limited to sending
+	//simple text PRIVMSGs back to the source of the
+	//incoming message.  For more complex behavior,
+	//Send(*Message) can be used.
+	if msg.Ctcp == "ACTION" {
+		var target string
+
+		if msg.Args[0] == bot.Nick {
+			target = msg.GetSender()
+		} else {
+			target = msg.Args[0]
+		}
+
+		bot.Send(&irc.Message{
+		Command : "PRIVMSG",
+		Args : []string{target},
+		Ctcp : "ACTION",
+		Trailing : msg.Trailing,
+		})
+	}
+	
+	return ""
 }
 
 //Function to join any channels the bot is invited to.
